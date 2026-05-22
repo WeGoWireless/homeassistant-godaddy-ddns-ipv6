c62c5e8ba6fccd3a9c8bdeab18580895107c8664
WeGoWireless.com <59068256+WeGoWireless@users.noreply.github.com>
2026-05-22 11:40:01 -0700

    Create const.py

---
+++ custom_components/godaddy_ddns_ipv6/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "godaddy_ddns_ipv6"
+
+CONF_API_KEY = "api_key"
+CONF_API_SECRET = "api_secret"
+CONF_DOMAIN = "domain"
+CONF_RECORD_NAME = "record_name"
+CONF_INTERFACE = "interface"
+CONF_TTL = "ttl"
+
+DEFAULT_INTERFACE = "end0"
+DEFAULT_TTL = 600
+DEFAULT_RECORD_NAME = "@"
+
+CONF_UPDATE_INTERVAL = "update_interval"
+DEFAULT_UPDATE_INTERVAL = 60
